35133001cb9a46a9673e1b0e29de9e9d6e52af10
Carlos Castillo <crc8109@pm.me>
2021-03-08 18:15:28 -0500

    Updated comments

--- ex3.py
+++ ex3.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+# Different reliefs
 border_effects = {
     "flat": tk.FLAT,
     "sunken": tk.SUNKEN,
@@ -13,6 +14,8 @@ window = tk.Tk()
 #iterates through dict border_effects to shopw user what they look like
 for relief_name, relief in border_effects.items():
     frame = tk.Frame(master=window, relief=relief, borderwidth=5)
+
+    #tells it to place it at the left most position
     frame.pack(side=tk.LEFT)
     label = tk.Label(master=frame, text=relief_name)
     label.pack()
