8d75e0717dbe46a2192d1a0eeb1517966c5b9fd8
crc8109 <crc8109@pm.me>
2021-03-09 21:11:43 -0500

    (feat) adds explanationatory comments

--- ex7.py
+++ ex7.py
@@ -3,7 +3,7 @@ import tkinter as tk
 
 window = tk.Tk()
 
-<<<<<<< HEAD
+# Create a 3*3 grid of frames
 for i in range(3):
     for j in range(3):
         frame = tk.Frame(
@@ -12,24 +12,8 @@ for i in range(3):
             borderwidth=1
         )
         frame.grid(row=i, column=j)
+        # Label each sub grid by it's y,x coordinates
         label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
         label.pack()
-=======
-
-frame = tk.Frame(master=window, width=150, height=150)
-
-frame.pack()
-
-
-label1 = tk.Label(master=frame, text="I'm at (0, 0)", bg="red")
-
-label1.place(x=0, y=0)
-
-
-label2 = tk.Label(master=frame, text="I'm at (75, 75)", bg="yellow")
-
-label2.place(x=75, y=75)
-
->>>>>>> 757e9f1839baadaba1d9c99c3e11c2b45283aa7b
 
 window.mainloop()
