757e9f1839baadaba1d9c99c3e11c2b45283aa7b
crc8109 <crc8109@pm.me>
2021-03-09 21:06:07 -0500

    (fix) correct ex7

--- ex7.py
+++ ex7.py
@@ -1,14 +1,22 @@
 import tkinter as tk
 
+
 window = tk.Tk()
 
-frame1 = tk.Frame(master=window, width=100, height=100, bg="red")
-frame1.pack()
 
-frame2 = tk.Frame(master=window, width=50, height=50, bg="yellow")
-frame2.pack()
+frame = tk.Frame(master=window, width=150, height=150)
 
-frame3 = tk.Frame(master=window, width=25, height=25, bg="blue")
-frame3.pack()
+frame.pack()
+
+
+label1 = tk.Label(master=frame, text="I'm at (0, 0)", bg="red")
+
+label1.place(x=0, y=0)
+
+
+label2 = tk.Label(master=frame, text="I'm at (75, 75)", bg="yellow")
+
+label2.place(x=75, y=75)
+
 
 window.mainloop()
