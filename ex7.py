81673a0f2bb686de3a6d39c27d67367ddb3c1d9b
crc8109 <crc8109@pm.me>
2021-03-09 21:24:24 -0500

    (feat) adds more comments and padding to labels and frames

--- ex7.py
+++ ex7.py
@@ -11,9 +11,18 @@ for i in range(3):
             relief=tk.RAISED,
             borderwidth=1
         )
-        frame.grid(row=i, column=j)
+        # Key operative here is grid(), used to attach frames to window
+        # Also pads frame in x & y direction in PIXELS
+        frame.grid(row=i, column=j, padx=5, pady=5)
         # Label each sub grid by it's y,x coordinates
-        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-        label.pack()
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")\
+        # Attaches each label to its master grame
+        # Also pads label in x & y direction in PIXELS
+        label.pack(padx=5, pady=5)
+
+# Insight: even though .grid() is called on each Frame object, the geometry
+# manager applies to the window object
+
+# Also, the layout of each frame is controlled with the .pack() geometry manager
 
 window.mainloop()
