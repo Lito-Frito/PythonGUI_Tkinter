90f6ad45d28791cd645d52341401a021ca6ab813
crc8109 <crc8109@pm.me>
2021-03-12 17:49:55 -0500

    Add comments explaiing code

--- ex10.py
+++ ex10.py
@@ -2,12 +2,23 @@ import tkinter as tk
 
 window = tk.Tk()
 window.columnconfigure(0, minsize=250)
-window.rowconfigure([0, 1], minsize=100)
+window.rowconfigure([0, 3], minsize=100)
 
 label1 = tk.Label(text="A")
+# Sets Label1 to stick to Northern border
 label1.grid(row=0, column=0, sticky="n")
 
 label2 = tk.Label(text="B")
+# Sets Label2 to stick to Northern border
 label2.grid(row=1, column=0, sticky="n")
+
+label3 = tk.Label(text="C")
+# Sets Label3 to stick to Northeastern border
+label3.grid(row=1, column=0, sticky="ne")
+
+label4 = tk.Label(text="C")
+# Sets Label4 to stick to Southwestern border
+label4.grid(row=1, column=0, sticky="sw")
+
 
 window.mainloop()
